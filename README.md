4f30cd45a130ce477b1cfb4ffecd3f6c04d995c1
Andrew Gotham <andrewgotham@hotmail.com>
2025-02-02 15:44:32 +0300

    initial commit with language model scaffolding

---
+++ README.md
@@ -0,0 +1,11 @@
+## GothamFolio
+
+Life changes from time to time. I've come to a next turn achieving natural limits in my current position and company.
+
+Doing same things day after day with no or very little progress or creativity does not sound right.
+
+Here comes the need for a personal portfolio Web site to display the skills, knowledge and experteese I am about to bring with me to a new employee.
+
+GothamFolio is a personal portfolio Web site reflecting skills and portfolios.
+
+The site is built with the current Laravel (version 11 at the time of start) and Blade with some of Alipnejs at the front-end. 
---
+++ bootstrap/app.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\SetLocale;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        then: function () {
+            Route::middleware(['web', 'auth'])
+                ->prefix('admin')
+                ->name('admin.')
+                ->group(base_path('routes/admin.php'));
+        }
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->appendToGroup('web', SetLocale::class);
+        $middleware->appendToGroup('web', ShareErrorsFromSession::class);
+        $middleware->alias([
+            'admin' => IsAdmin::class
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
