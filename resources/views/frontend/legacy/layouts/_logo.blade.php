219424461b445b5b98fbbfbe1b64de4f4d3fa571
AndrewGotham <andrewgotham@hotmail.com>
2025-02-11 09:28:02 +0300

    logo image and social icons/links

--- resources/views/frontend/legacy/layouts/_logo.blade.php
+++ resources/views/frontend/legacy/layouts/_logo.blade.php
@@ -1,9 +1,9 @@
 @if(Request::is('/'))
-    <span class="navbar-brand">
-        <div class="css-logo"></div>AndrewGotham
+    <span class="navbar-brand row">
+        <img src="{{ asset('assets/frontend/legacy/img/ag-logo.png') }}" class="pull-left" style="margin-right: 6px" width="28px" /> {{--<div class="css-logo"></div>--}}<span class="pull-left">AndrewGotham</span>
     </span>
 @else
-    <a href="{{ route('home') }}" class="navbar-brand">
-        <div class="css-logo"></div>AndrewGotham
+    <a href="{{ route('home') }}" class="navbar-brand row items-center">
+        <img src="{{ asset('assets/frontend/legacy/img/ag-logo.png') }}" class="pull-left" width="28px" style="margin-right: 6px" /> {{--<div class="css-logo"></div>--}}<span class="pull-left ml-4 align-middle">AndrewGotham</span>
     </a>
 @endif
